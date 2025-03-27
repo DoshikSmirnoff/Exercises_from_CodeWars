476992d1b894102e7b433780e16620279b78e02b
Daria <DoshikSmirnoff@gmail.com>
2025-03-27 17:36:50 +0300

    COMMIT #3: Changes in Exercise_#3

--- In process/Exercise_#3.py
+++ In process/Exercise_#3.py
@@ -6,18 +6,20 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
 """
 
-def sum_of_two_smallest_numbers(start_list: list) -> list:
-    #start_list = [1, 10, 100, 120, 130, 140, 2]
-    spisok_new = []
+def sum_of_two_smallest_numbers(start: list) -> int:
 
-    for char in start_list:
-        if char < 0 or isinstance(char, int) == False:
-            print("Числа в списке должны быть положительными и целыми!")
-        else:
-            spisok_new += char
-            spisok_sorted = sorted(spisok_new)
+#    start = [1, 10, 100, 120, 130, 140, 2]
+#    spisok_new = []
 
-    print(spisok_sorted)
+#    for char in start:
+#       if char < 0 or isinstance(char, int) == False:
+#            return "Числа в списке должны быть положительными и целыми!"
+#       else:
+#            # spisok_new.append(char) #None - ?
+#           spisok_sorted = sorted(spisok_new.append(char))
+#
+#            return spisok_sorted
+    sorted_start = sorted(start, reverse=False)
+    sum_of_nums = sorted_start[0] + sorted_start[1]
 
-
-sum_of_two_smallest_numbers(start_list = [1, 10, 100, 120, 130, 140, 2]) # TypeError: 'int' object is not iterable
+print(sum_of_two_smallest_numbers([1, 10, 100, 120, 130, 140, 2]))
